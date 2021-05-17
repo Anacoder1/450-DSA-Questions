47b1973836a5a5c2621197cb3648e1d17130e24e
Manendra Nath Shukla <51334809+manendranathshukla@users.noreply.github.com>
2021-05-17 08:25:49 +0545

    New Approach Using O(N)

--- python/Array/min_max_elements_array.py
+++ python/Array/min_max_elements_array.py
@@ -4,3 +4,16 @@ def min_max_elements(A):
     minimum_element = A[0]
     maximum_element = A[-1]
     return (minimum_element, maximum_element)
+
+
+#Approach 2   - TC => O(N)
+
+def max_min_elements(A):
+    myMax=A[0]
+    myMin=A[0]
+    for d in A:
+        if(d> myMax):
+            myMax=d
+        if(d<myMin):
+            myMin=d 
+    return(myMin,myMax)
